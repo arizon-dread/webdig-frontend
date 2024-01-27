9ec08ed14505c80e56927312cda0b0165d66d961
Erik Svensson <arizon.es@gmail.com>
2024-01-27 17:17:25 +0100

    fix layout and add button for reverse ip lookup

--- src/app/dns-lookup/dns-lookup.component.ts
+++ src/app/dns-lookup/dns-lookup.component.ts
@@ -25,14 +25,20 @@ export class DnsLookupComponent {
   form = this.fb.group({
     searchField: ['', Validators.required]
   });
+  searching = false;
 
   resp: LookupResponse | undefined;
 
   constructor(private fb: FormBuilder, private lookupSvc: LookupService, private errHandler: ErrorHandlerService) {}
+  doReverseLookup(ip: string) {
+    this.form.controls["searchField"].setValue(ip);
+    this.lookupDNS();
+  }
 
   lookupDNS() {
     if (this.form.valid) {
       this.resp = undefined;
+      this.searching = true;
       this.form.controls['searchField'].setValue(this.form.controls['searchField'].value?.trim() ?? "")
       const req: LookupRequest = {
         host: this.form.controls['searchField'].value ?? ""
@@ -42,10 +48,12 @@ export class DnsLookupComponent {
           if (data) {
             this.resp = data;
           }
+          this.searching = false;
         },
         error: (err: Error) => {
           //this.errHandler.displayMsgToUser("Unable to get proper lookup from backend", ToastrType.error);
           console.log(err);
+          this.searching = false;
         }
       });
 
