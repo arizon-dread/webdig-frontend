ab5436097a923658b17b13e6f858ec83b5219fc4
Erik Svensson <arizon.es@gmail.com>
2024-11-25 22:40:59 +0100

    add match rule for all routes to dnsLookupComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { DnsLookupComponent } from './dns-lookup/dns-lookup.component';
 
 export const routes: Routes = [
-    {path: '', component: DnsLookupComponent}
+    {path: '**', component: DnsLookupComponent}
 ];
