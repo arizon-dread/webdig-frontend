d853cf9888d0d38fc2180ed905bb2bdcb9e61385
Erik Svensson <erik.svensson@regionorebrolan.se>
2025-11-25 13:37:20 +0100

    add version service for footer

---
+++ src/app/services/version.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Version } from '../models/version';
+import { ConfigService } from './config.service';
+import { ErrorHandlerService } from './error-handler.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VersionService {
+
+  constructor(private http: HttpClient, private errHandler: ErrorHandlerService) {}
+
+  getFrontendVersion(): Observable<Version> {
+    return this.http.get<Version>("/version");
+  }
+  getBackendVersion(): Observable<Version> {
+    return this.http.get<Version>(ConfigService.config.apiBaseUrl +'/version')
+  }
+}
